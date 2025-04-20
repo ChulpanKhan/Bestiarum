366f070a27edd1e62b256cfbba4b3549bc430533
ChulpanKhan <chulpan.khannanova@gmail.com>
2025-04-21 00:53:32 +0300

    Начало гуи

---
+++ Monsters/src/main/java/com/mycompany/monsters/GUI/FileChooser.java
@@ -0,0 +1,43 @@
+
+package com.mycompany.monsters.GUI;
+
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class FileChooser {
+        
+    public static File selectImportFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Выберите файл");
+        fileChooser.setMultiSelectionEnabled(true);
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "Поддерживаемые форматы (XML, JSON, YAML)",
+                "xml", "json", "yml", "yaml");
+        fileChooser.setFileFilter(filter);
+
+        int userSelection = fileChooser.showOpenDialog(null);
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            return fileChooser.getSelectedFile();
+        }
+        return null;
+    }
+    
+        public static File selectExportFile(String suffix) {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Выберите место для экспорта файла");
+        fileChooser.setFileFilter(new FileNameExtensionFilter("Excel Files", suffix));
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+        int userSelection = fileChooser.showSaveDialog(null);
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            String filePath = selectedFile.getAbsolutePath();
+            if (!filePath.endsWith(suffix)) {
+                selectedFile = new File(filePath + suffix);
+            }
+            return selectedFile;
+        }
+        return null;
+    }
+}
